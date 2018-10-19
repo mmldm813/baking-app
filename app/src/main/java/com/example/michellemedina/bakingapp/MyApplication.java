27556a0ae942b01a0c94c3a82af71922247a1042
michellemedina <michelle200med@gmail.com>
2018-10-19 15:01:01 -0400

    BAK-6 - Add Dagger

---
+++ app/src/main/java/com/example/michellemedina/bakingapp/MyApplication.java
@@ -0,0 +1,21 @@
+package com.example.michellemedina.bakingapp;
+
+import android.app.Application;
+
+import com.example.michellemedina.bakingapp.dagger.ApplicationComponent;
+import com.example.michellemedina.bakingapp.dagger.ApplicationModule;
+import com.example.michellemedina.bakingapp.dagger.DaggerApplicationComponent;
+
+public class MyApplication extends Application {
+
+    private ApplicationComponent applicationComponent;
+
+    public ApplicationComponent getComponent() {
+        if (applicationComponent == null) {
+            applicationComponent = DaggerApplicationComponent.builder()
+                    .applicationModule(new ApplicationModule(this))
+                    .build();
+        }
+        return applicationComponent;
+    }
+}
