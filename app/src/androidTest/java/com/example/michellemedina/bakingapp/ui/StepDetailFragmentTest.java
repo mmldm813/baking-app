925c03e65b94612fd3422e162164d03c4e008fb2
michellemedina <michelle200med@gmail.com>
2018-11-08 12:13:26 -0500

    added test that checks if exoplayer is displayed
    added test that checks if image replacing exoplayer is displayed

--- app/src/androidTest/java/com/example/michellemedina/bakingapp/ui/StepDetailFragmentTest.java
+++ app/src/androidTest/java/com/example/michellemedina/bakingapp/ui/StepDetailFragmentTest.java
@@ -7,6 +7,7 @@ import com.example.michellemedina.bakingapp.MockServerDispatcher;
 import com.example.michellemedina.bakingapp.R;
 import com.example.michellemedina.bakingapp.data.Dessert;
 import com.example.michellemedina.bakingapp.detail.RecipeDetailActivity;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -24,10 +25,13 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 
 public class StepDetailFragmentTest {
@@ -94,6 +98,24 @@ public class StepDetailFragmentTest {
                 "cracker crumbs, 50 grams (1/4 cup) of sugar, and 1/2 teaspoon of salt together " +
                 "in a medium bowl. Pour the melted butter and 1 teaspoon of vanilla into the dry " +
                 "ingredients and stir together until evenly mixed.")));
+    }
+
+    @Test
+    public void checkExoplayerIsDisplayed() {
+        webServer.setDispatcher(new MockServerDispatcher(null, 200, fakeData).new RequestDispatcher());
+        activityTestRule.launchActivity(new Intent().putExtra("dessert", desserts.get(0)));
+
+        onView(allOf(withId(R.id.short_description), withText("Recipe Introduction"))).perform(scrollTo()).perform(click());
+        onView(allOf(withId(R.id.player_view), withClassName(is(SimpleExoPlayerView.class.getName()))));
+    }
+
+    @Test
+    public void checkPhotoReplacesExoplayerWhenNeeded() {
+        webServer.setDispatcher(new MockServerDispatcher(null, 200, fakeData).new RequestDispatcher());
+        activityTestRule.launchActivity(new Intent().putExtra("dessert", desserts.get(0)));
+
+        onView(allOf(withId(R.id.short_description), withText("Starting prep"))).perform(scrollTo()).perform(click());
+        onView(withId(R.id.no_video_image)).check(matches(isDisplayed()));
     }
 
     @After
